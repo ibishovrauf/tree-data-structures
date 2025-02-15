58d84859ec9bc224a05cfc307464febced939e6a
ibishovrauf <raufibisov305@gmail.com>
2025-02-15 18:06:26 +0400

    Trie node implementation

--- src/nodes/trie_node.py
+++ src/nodes/trie_node.py
@@ -1,18 +1,70 @@
 from typing import Any, Dict, Optional
-from .base_node import BaseNode
+from copy import deepcopy
+
+from src.nodes.base_node import BaseNode
+
 
 class TrieNode(BaseNode):
-    def __init__(self, value: Any, childs: Optional[Dict[str, "TrieNode"]] = {}) -> None:
+
+    def __init__(
+                self,
+                value: Any,
+                children: Dict[str, "TrieNode"] = dict()
+            ) -> None:
         super().__init__(value)
-        self._childs = childs
+        self._children = deepcopy(children)
+        self._wordEnd = False
 
     @property
-    def childs(self):
-        return self._childs.copy()
+    def children(self) -> Dict[str, "TrieNode"]:
+        return self._children
+
+    @children.setter
+    def children(self, value: Dict[str, "TrieNode"]) -> None:
+        """Set the child nodes after validating the input dictionary."""
+        if not isinstance(value, dict):
+            raise TypeError("children must be a dictionary.")
+        for key, node in value.items():
+            if not isinstance(key, str):
+                raise TypeError("All keys in children must be strings.")
+            if not isinstance(node, TrieNode):
+                raise TypeError(
+                    "All values in children must be TrieNode instances.")
+        self._children = value
+
+    @property
+    def wordEnd(self):
+        return self._wordEnd
+
+    @wordEnd.setter
+    def wordEnd(self, value: bool):
+        if not isinstance(value, bool):
+            raise TypeError("wordEnd must be a boolean")
+        self._wordEnd = value
+
+    @property
+    def child_count(self) -> int:
+        return len(self._children.keys())
+
+    def add_child(self, value: Dict[str, "TrieNode"]) -> None:
+        if not isinstance(value, dict):
+            raise TypeError("children must be a dictionary.")
+
+        for key, node in value.items():
+            if not isinstance(key, str):
+                raise TypeError("All keys in children must be strings.")
+
+            if not isinstance(node, TrieNode):
+                raise TypeError(
+                    "All values in children must be TrieNode instances.")
+
+            self._children[key] = node
 
     def __str__(self) -> str:
-        return f"Node({self.value}) with {len(self.childs)} children"
+        children_str = ", ".join(key for key in self._children.keys())
+        return f"Node({self.value}) with {len(self.children)} children: {children_str}"
 
     def __repr__(self) -> str:
-        childs_str = ", ".join(f"{key}: {value}" for key, value in self._childs.items())
-        return f"Node(value={self._value}, childs={childs_str})"
+        children_str = ", ".join(f"{key}: {value}"
+                               for key, value in self._children.items())
+        return f"Node(value={self._value}, children={children_str})"
