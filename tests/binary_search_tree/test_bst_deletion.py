867f7d48f15c10958dbcd5d3e7f8100e4b6ef02e
ibishovrauf <raufibisov305@gmail.com>
2025-03-23 11:56:39 +0400

    BSTree add test for deleting all nodes

--- tests/binary_search_tree/test_bst_deletion.py
+++ tests/binary_search_tree/test_bst_deletion.py
@@ -16,3 +16,21 @@ def test_delete_with_one_child(populated_bst_2):
     populated_bst_2.delete(10)
     assert populated_bst_2.search(9).right.value == 11
     assert populated_bst_2.search(12).left == None
+
+def test_delete_all_nodes(populated_bst):
+    """Test deleting all nodes from the tree"""
+    values = []
+    def collect_values(node):
+        if node:
+            collect_values(node.left)
+            values.append(node.value)
+            collect_values(node.right)
+    
+    collect_values(populated_bst.root)
+    
+    # Delete all values
+    for value in values[::-1]:
+        print(f"{value} deleted")
+        populated_bst.delete(value)
+    # Tree should be empty
+    assert populated_bst.root is None
